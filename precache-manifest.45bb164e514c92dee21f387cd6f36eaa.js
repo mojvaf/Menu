899e0eb20a1cdb80c66024a4eb2970225b71dcec
mojvaf <mvafa44@gmail.com>
2020-09-18 23:48:05 +0200

    Updates

---
+++ precache-manifest.45bb164e514c92dee21f387cd6f36eaa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52915235aba9400707b93cb71224f47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "014d66076e1ce14ce8a2",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3f0963cdce77193b7aa7",
+    "url": "./static/css/main.8b17d144.chunk.css"
+  },
+  {
+    "revision": "014d66076e1ce14ce8a2",
+    "url": "./static/js/2.f2ef32f3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.f2ef32f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0963cdce77193b7aa7",
+    "url": "./static/js/main.c628498f.chunk.js"
+  },
+  {
+    "revision": "e5ea12d44b57d0e12ac2",
+    "url": "./static/js/runtime-main.5fbd3476.js"
+  },
+  {
+    "revision": "40d59b519855f6ddf74f060f3fa778b6",
+    "url": "./static/media/carousel1.40d59b51.jpg"
+  },
+  {
+    "revision": "241e48bdff2f592320756438ae5d0236",
+    "url": "./static/media/carousel2.241e48bd.jpg"
+  },
+  {
+    "revision": "be49e8778a08c8c870bf75b7bd24e421",
+    "url": "./static/media/challenge.be49e877.jpg"
+  },
+  {
+    "revision": "9ea23cd028d32c3848a18a011a46751e",
+    "url": "./static/media/open.9ea23cd0.jpg"
+  },
+  {
+    "revision": "7c5f0fce2cf3e82620753d176dcc1369",
+    "url": "./static/media/sign.7c5f0fce.jpg"
+  }
+]);
